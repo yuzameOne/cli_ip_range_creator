dfd16f7fbadc1d1d0f534abbfa348aa3f2052108
yuzameOne <E.homayko@yandex.ru>
2021-09-27 00:57:03 +1000

    write one function, and update TODO

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ var newCustomString strings.Builder
 var subSliceStringBuilder []string
 var sliceOfSlice [][]string
 var stringRange []string
+var rangeMap = make(map[string]string, len(stringRange))
 
 func readIpRangeFile(pathToFle string) []string {
 
@@ -26,7 +27,7 @@ func readIpRangeFile(pathToFle string) []string {
 	r := bufio.NewReader(f)
 
 	for {
-		line, err := r.ReadString(10)
+		line, err := r.ReadString('\n')
 
 		if err == io.EOF {
 			break
@@ -39,16 +40,31 @@ func readIpRangeFile(pathToFle string) []string {
 
 //TODO:
 
-// идея с могомерным массивом не удалась (((((
-// переписать название  фукции  clearExtraChar
-
-// что думаю :
-
-//  в фукции ниже изменить название на splitRange и писать это все в map[[]string][]string и в следущей функции уже.
-
+//  все это в switchRange
 //  разбить строку Split вернет []string, strconv сравивать конвертить  и тут же откручивать
+// 	возможно копировать по индексу (copy) концы строки
 
-func clearExtraChar(x func(string) []string) {
+func splitRange(x func(string) []string) map[string]string {
+
+	for i := 0; i < len(stringRange); i++ {
+
+		str := stringRange[i]
+
+		subSlice := strings.Split(str, "-")
+
+		for i := 0; i < len(stringRange); i++ {
+
+			rangeMap[subSlice[0]] = subSlice[1]
+			break
+		}
+
+	}
+
+	return rangeMap
+
+}
+
+func switchRange(x func(map[string]string)) {
 
 }
 
@@ -56,5 +72,5 @@ func main() {
 
 	readIpRangeFile("example.txt")
 
-	clearExtraChar(readIpRangeFile)
+	splitRange(readIpRangeFile)
 }
