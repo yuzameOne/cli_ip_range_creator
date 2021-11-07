b79f5beb4f64baa7b218e2ea65303daf11520d53
yuzameOne <E.homayko@yandex.ru>
2021-11-07 23:32:55 +1000

    add var Threads

--- main.go
+++ main.go
@@ -4,7 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"runtime"
 )
+
+var THREADS int = runtime.GOMAXPROCS(0)
+
+/*
+	TODO
+
+	так  будет fanIn -> fanOut
+
+	для чего нужна  переменая gomaxprocs ???? для того чтобы задейвствовать все потоки которые есть у планировщика на проце автоматически
+
+	в моем случае это 4 потока(ноут), дома настольный ПК  16 потоков.
+
+	
+
+
+*/
 
 func readFile(fname string) {
 
